27a79a9bba75bdfe6e3cdc1a32b77faae8ed10a1
Tobias Gassmann <tobias@gaszmann.de>
2015-05-13 16:48:55 +0200

    First commit

---
+++ app/src/main/java/buzz/bounded/bounded_buzz_client/MainActivity.java
@@ -0,0 +1,54 @@
+package buzz.bounded.bounded_buzz_client;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickSendMessageButton(View button) {
+        final EditText messageTextView = (EditText) findViewById(R.id.editBoundedMessage);
+        final String messageText = messageTextView.getText().toString();
+        Log.i("some",messageText);
+    }
+
+    public void onClickSettingsButton(View button){
+        final Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+}
